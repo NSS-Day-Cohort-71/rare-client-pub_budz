5852f7a9058d34a1b79cef8f3785328ce0567e97
Jeremy Whitney <bearwhitness@gmail.com>
2024-08-29 14:52:35 -0500

    Deleted commented out version of updatePost in postService

--- src/services/postService.js
+++ src/services/postService.js
@@ -23,38 +23,27 @@ export const getPostById = async (postId) => {
   }
 };
 
-// export const updatePost = async (postId, updatedPost) => {
-//   try {
-//     const res = await fetch(`http://localhost:8088/posts/${postId}`, {
-//       method: "PUT",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(updatedPost),
-//     });
-
-//     if (res.status === 204) {
-//       return {};
-//     }
-
-//     if (!res.ok) {
-//       throw new Error(`Error updating post: ${res.statusText}`);
-//     }
-
-//     return await res.json();
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return null;
-//   }
-// };
 export const updatePost = async (postId, updatedPost) => {
-  console.log('Updating post with ID:', postId);
-  console.log('Post data:', updatedPost);
-  return fetch(`http://localhost:8088/posts/${postId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedPost),
-  }).then((res) => res.json());
+  try {
+    const res = await fetch(`http://localhost:8088/posts/${postId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedPost),
+    });
+
+    if (res.status === 204) {
+      return {};
+    }
+
+    if (!res.ok) {
+      throw new Error(`Error updating post: ${res.statusText}`);
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
 };
